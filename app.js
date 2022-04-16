0431906b55670ab9cce9d0c01f20c0073f1780ba
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-04-16 13:07:13 -0700

    set up mongoose

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +9,13 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = `mongodb+srv://bawa:${process.env.mongoPass}@cluster0.e8ih0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+mongoose.connect(mongoDB, { useNewUrlParser:true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
