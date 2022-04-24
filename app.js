56329ab35e82e6410978a46163474134b95c8d6a
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-04-23 17:52:01 -0700

    added packages for production

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var compression  = require('compression')
+var helmet = require('helmet');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
+
+app.use(compression());
+app.use(helmet());
 
 
 var app = express();
