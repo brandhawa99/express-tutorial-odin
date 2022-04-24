beb4ff0cb42f8297072cd5bb35db0ac062933da8
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-04-23 18:21:20 -0700

    asdf

--- app.js
+++ app.js
@@ -23,10 +23,6 @@ mongoose.connect(mongoDB, { useNewUrlParser:true, useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-
-app.use(compression());
-app.use(helmet());
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -40,6 +36,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+app.use(compression());
+app.use(helmet());
 
 
 // catch 404 and forward to error handler
